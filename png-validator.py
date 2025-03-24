d2309dffb4ea88b9682b53276b9e230d89649fd2
Daniel Porath <u8842595@bsmch.net>
2025-03-24 14:26:00 +0200

    Fix(png-validator.py): fix validation deprecated code

--- png-validator.py
+++ png-validator.py
@@ -1,29 +1,27 @@
 def validate_png(image_path):
     PNG_MAGIC_NUMBER = "89504e470d0a1a0a"
     PNG_MAGIC_NUMBER_SIZE = len(PNG_MAGIC_NUMBER)
+
+    cursor_0 = 0
  
     with open(image_path, 'r+b') as image:
         hexData = image.read().hex()
  
-         #check signature (8 bytes)
+        #check signature / magic bytes
         start = 0
         stop = cursor_0+(8*2)
         cursor_0 = stop
         if hexData[start:stop] != "89504e470d0a1a0a":
             print("signature fail")
-        else:
-            print("signature succeeded")
- 
- 
-        # check png signature / magic number
-        if(image.read(PNG_MAGIC_NUMBER_SIZE) != PNG_MAGIC_NUMBER):
             return False
         else:
+            print("signature succeeded")
             return True
+ 
     return True
  
  
  
 # usage examples
-#print(validate_png("C:\\Users\miunim132\Downloads\jpgExample.jpg"))
-#print(validate_png("C:\\Users\miunim132\Downloads\cubes.png"))
+print(validate_png("C:\\Users\DanielPorath\Documents\TEAM-SAGOL\images\\beach.jpg"))
+print(validate_png("C:\\Users\DanielPorath\Documents\TEAM-SAGOL\images\\mario.png"))
