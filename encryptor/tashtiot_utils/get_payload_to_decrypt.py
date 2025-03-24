1c6c70c6378940b1f05511581deaf49a1efba944
Daniel Maier <u9247741@bsmch.net>
2025-03-24 15:01:43 +0200

    hotFix(get_payload_to_encrypt,get_payload_to_decrypt):variable name pkt

--- encryptor/tashtiot_utils/get_payload_to_decrypt.py
+++ encryptor/tashtiot_utils/get_payload_to_decrypt.py
@@ -3,4 +3,4 @@ from scapy.all import *
 # Extract encrypted payload from packet to decrypt as bytes/string. 
 # Note: Logic depends on which headers were encrypted too
 def get_payload_to_decrypt(pkt):
-    return bytes(packet[IP].payload)
+    return bytes(pkt[IP].payload)
