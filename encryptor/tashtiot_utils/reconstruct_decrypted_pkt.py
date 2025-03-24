8f7b711c1584af0f658ed46cea305386580db732
Ran Yaffe <u9324418@bsmch.net>
2025-03-24 13:55:39 +0200

    feat(reconstruct_encrypted_pkt.py): feat reconstruct_encrypted_pkt function

--- encryptor/tashtiot_utils/reconstruct_decrypted_pkt.py
+++ encryptor/tashtiot_utils/reconstruct_decrypted_pkt.py
@@ -1,6 +1,8 @@
-from scapy.all import *
+# from scapy.all import *
 
-# Reconstruct packet with decrypted payload bytes to send out
-# With updated addresses
-def reconstruct_decrypted_pkt(decrypted_payload):
-    return "Replace me with a packet!"
+# # Reconstruct packet with decrypted payload bytes to send out
+# # With updated addresses
+# def reconstruct_decrypted_pkt(decrypted_payload):
+#     pkt = Ether(src = "ff:ff:ff:ff:ff:ff", dst="00:B0:D0:63:C2:26") / IP(src = "196.168.1.100", dst="12.0.0.1") / decrypted_payload # Create packet with decrypted payload
+#     pkt[Raw].load = decrypted_payload # Update payload
+#     return pkt
