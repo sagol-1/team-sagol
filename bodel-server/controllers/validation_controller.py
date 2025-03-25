887a53c635294db5395379e45065ef78757feb47
miunim 26 <miunim26@bsmch.net>
2025-03-25 16:15:29 +0200

    feat(validation_controller.py, validation_service.py): replaced file list received from request with fixed files

--- bodel-server/controllers/validation_controller.py
+++ bodel-server/controllers/validation_controller.py
@@ -1,14 +1,11 @@
-from typing import List
 from fastapi import UploadFile, APIRouter
 from services.validation_service import validation_service
 
 validation_router = APIRouter()
 
-@validation_router.post("/multi")
-async def check(files: List[UploadFile]) -> object:
+@validation_router.post("/image")
+async def postImage(json_file: UploadFile, png: UploadFile) -> object:
     try:
-        return validation_service(files)
+        return validation_service(json_file, png)
     except Exception as error:
         return error
-    
-    return {"state": 200}
--- bodel-server/services/validation_service.py
+++ bodel-server/services/validation_service.py
@@ -1,18 +1,22 @@
-from typing import List
 from fastapi import HTTPException, UploadFile
 from filters.png_filter import png_validation
 from filters.jsonValidate import validateJsonToSchemas
 
 
-def validation_service(files: List[UploadFile]) -> None: 
+def validation_service(json_file: UploadFile, png: UploadFile) -> None:
+    is_valid = True
+
     try:
-        isValid = True
-        for file in files:
-            if file.filename.lower().endswith('.png'):
-                isValid = isValid and png_validation(file.file.read())
-            elif file.filename.lower().endswith('.json'):
-                isValid = isValid and validateJsonToSchemas(file.file.read())
-        return isValid
+        if png.filename.lower().endswith('.png'):
+            is_valid = is_valid and png_validation(png.file.read())
+        else:
+            is_valid = False
+        if json_file.filename.lower().endswith('.json'):
+            is_valid = is_valid and validateJsonToSchemas(json_file.file.read())
+        else:
+            is_valid = False
+
+        return is_valid
     except:
-        raise HTTPException(status_code=500, detail=" -> Something went wrong")
+        raise HTTPException(status_code=500)
     
