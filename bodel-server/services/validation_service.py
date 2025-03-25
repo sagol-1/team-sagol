d746dc5490af1062559f6f05a5eee9950d29b41c
miunim 26 <miunim26@bsmch.net>
2025-03-25 17:24:46 +0200

    fix(validation_service.py): fixed validation calls

--- bodel-server/services/validation_service.py
+++ bodel-server/services/validation_service.py
@@ -4,25 +4,13 @@ from filters.jsonValidate import validateJsonToSchemas
 
 
 def validation_service(json: UploadFile, png: UploadFile) -> None:
-    is_valid = True
     print("before png")
 
     try:
-        if png.filename.lower().endswith('.png'):
-            is_valid = is_valid and png_validation(png.file.read())
+        if png.filename.lower().endswith('.png') and json.filename.lower().endswith('.json'):
+            return png_validation(png.file) and validateJsonToSchemas(json.file.read())
         else:
-            is_valid = False
-        
-        print("png: " + is_valid)
-
-        if json.filename.lower().endswith('.json'):
-            is_valid = is_valid and validateJsonToSchemas(json.file.read())
-        else:
-            is_valid = False
-        
-        print("json: " + is_valid)
-
-        return is_valid
-    except:
-        raise HTTPException(status_code=500)
+            return False
+    except Exception as error:
+        return HTTPException(status_code=500, detail="Something went wrong")
     
