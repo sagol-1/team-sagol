0c60d7c0fa70643065b04505efc6d90167f1df9b
Daniel Maier <u9247741@bsmch.net>
2025-03-25 16:18:48 +0200

    del(reconstruct_decryted_pkt.py):remove test prints

--- encryptor/tashtiot_utils/reconstruct_decrypted_pkt.py
+++ encryptor/tashtiot_utils/reconstruct_decrypted_pkt.py
@@ -5,11 +5,6 @@ import ast
 # With updated addresses
 def reconstruct_decrypted_pkt(decrypted_payload, SRC_IP, DST_IP):
     decrypted_payload_bytes = ast.literal_eval(decrypted_payload)
-    print("+=============================+")
-    print(decrypted_payload_bytes)
-    print("+=============================+")
-    print(type(decrypted_payload_bytes))
-    print("+=============================+")
     return Ether()/ \
             IP(src=SRC_IP, dst=DST_IP)/ \
                 TCP(decrypted_payload_bytes)
