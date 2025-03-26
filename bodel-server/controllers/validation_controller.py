f3573d76581727ab87ba8c8c3266211c6b3ffb67
miunim 26 <miunim26@bsmch.net>
2025-03-26 12:08:56 +0200

    fix(validation_service.py, validation_controller.py): fix response content

--- bodel-server/controllers/validation_controller.py
+++ bodel-server/controllers/validation_controller.py
@@ -1,4 +1,4 @@
-from fastapi import UploadFile, APIRouter, HTTPException, status
+from fastapi import UploadFile, APIRouter, Response, HTTPException, status
 from services.validation_service import validation_service
 
 validation_router = APIRouter()
@@ -6,6 +6,8 @@ validation_router = APIRouter()
 @validation_router.post("/image")
 async def postImage(json: UploadFile, png: UploadFile) -> object:
     try:
-        return validation_service(json, png)
+        await validation_service(json, png)
+        return Response(status_code=status.HTTP_200_OK)
     except Exception as error:
-        return error
+        if isinstance(error, HTTPException):
+            return Response(status_code=error.status_code)
