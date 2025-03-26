dbd6cff5b72fdc2f3c6cf2819475549f9a06b8a7
miunim 26 <miunim26@bsmch.net>
2025-03-26 10:59:10 +0200

    feat(validation_service.py, validation_controller.py): implement post request and refine error handling

--- bodel-server/controllers/validation_controller.py
+++ bodel-server/controllers/validation_controller.py
@@ -1,4 +1,4 @@
-from fastapi import UploadFile, APIRouter
+from fastapi import UploadFile, APIRouter, HTTPException, status
 from services.validation_service import validation_service
 
 validation_router = APIRouter()
--- bodel-server/services/validation_service.py
+++ bodel-server/services/validation_service.py
@@ -1,16 +1,24 @@
-from fastapi import HTTPException, UploadFile
+from fastapi import HTTPException, Response, UploadFile, status
+import httpx
 from filters.png_filter import png_validation
 from filters.jsonValidate import validateJsonToSchemas
+
+URL = "http://10.50.16.11:8000/data"
 
 
 def validation_service(json: UploadFile, png: UploadFile) -> None:
     print("before png")
 
     try:
-        if png.filename.lower().endswith('.png') and json.filename.lower().endswith('.json'):
-            return png_validation(png.file) and validateJsonToSchemas(json.file.read())
+        is_type_correct = png.filename.lower().endswith('.png') and json.filename.lower().endswith('.json')
+        is_files_valid = png_validation(png.file) and validateJsonToSchemas(json.file.read())
+        if is_type_correct and is_files_valid:
+                send_data(png, json)
         else:
-            return False
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Files invalid.")
+        return "Files sent."
     except Exception as error:
-        return HTTPException(status_code=500, detail="Something went wrong")
-    
+        return HTTPException(status_code=500, detail="Something went wrong -> " + error)
+
+def send_data(png: UploadFile, json: UploadFile):
+    httpx.post(URL, data={'png': png, 'json': json})
