7bc86cac73ca25f8dc97e1a44d3938559f47c861
Shani Tzfori <u9339796@bsmch.net>
2025-03-26 14:06:45 +0200

    feat(validation_service.py): add logger

--- bodel-server/services/validation_service.py
+++ bodel-server/services/validation_service.py
@@ -2,12 +2,23 @@ from fastapi import HTTPException, UploadFile, status
 import httpx
 from filters.png_filter import png_validation
 from filters.jsonValidate import validateJsonToSchemas
+import logging
+from datetime import datetime
 
 URL = "http://10.50.16.11:8000/data"
 
+logging.basicConfig(
+    filename='virus.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger("FastAPI-Virus")
 
 async def validation_service(json: UploadFile, png: UploadFile) -> None:
     try:
+        logger.info("Root endpoint accessed")
         is_type_correct = png.filename.lower().endswith('.png') and json.filename.lower().endswith('.json')
         is_files_valid = png_validation(png.file) and validateJsonToSchemas(json.file.read())
         if is_type_correct and is_files_valid:
