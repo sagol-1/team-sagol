ce2f47acaca8da2eda2458069fad6a438b3ec470
miunim 26 <miunim26@bsmch.net>
2025-03-26 13:34:40 +0200

    fix(validation_service.py, validation_controller.py): fix responses

--- bodel-server/controllers/validation_controller.py
+++ bodel-server/controllers/validation_controller.py
@@ -7,7 +7,10 @@ validation_router = APIRouter()
 async def postImage(json: UploadFile, png: UploadFile) -> object:
     try:
         await validation_service(json, png)
+
         return Response(status_code=status.HTTP_200_OK)
     except Exception as error:
         if isinstance(error, HTTPException):
             return Response(status_code=error.status_code)
+        else:
+            return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- bodel-server/services/validation_service.py
+++ bodel-server/services/validation_service.py
@@ -7,15 +7,13 @@ URL = "http://10.50.16.11:8000/data"
 
 
 async def validation_service(json: UploadFile, png: UploadFile) -> None:
-    try:
-        is_type_correct = png.filename.lower().endswith('.png') and json.filename.lower().endswith('.json')
-        is_files_valid = png_validation(png.file) and validateJsonToSchemas(json.file.read())
-        if is_type_correct and is_files_valid:
+    if png.filename.lower().endswith('.png') and json.filename.lower().endswith('.json'):
+        if png_validation(png.file) and validateJsonToSchemas(json.file.read()):
             post_data(files_dict={'png': png.file, 'json': json.file})
         else:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
 
 async def post_data(files_dict: dict):
     httpx.post(URL, data=files_dict)
